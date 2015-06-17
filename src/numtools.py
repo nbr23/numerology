62c74973d834617e00c55b0f9bd1a979a560e6e5
Maxence <max@23.tf>
2015-06-17 21:11:37 +0100

    Lexical permutation generation

--- src/numtools.py
+++ src/numtools.py
@@ -16,3 +16,30 @@ def lcopy(l):
     for i in range(0, len(l)):
         nl.insert(i, l[i])
     return nl
+
+def lex_perm(l, b):
+    l = lcopy(l)
+    x = -1
+    y = -1
+    ll = len(l)
+    lr = []
+    for i in range(0, ll - 1):
+        if (l[i] < l[i + 1]):
+            x = i
+    if x == -1:
+        return
+
+    for i in range(0, ll):
+        if l[x] < l[i]:
+            y = i
+    l[x],l[y] = l[y],l[x]
+
+    i = 0
+    while i <= x:
+        lr.append(l[i])
+        i += 1
+
+    for i in range(0, ll - x - 1):
+        lr.append(l[ll - 1 - i])
+    b.append(lr)
+    lex_perm(lr, b)
