5e9f1f85fbc0c20e98ef0655175e23d07ef0f65d
Maxence <max@23.tf>
2015-06-23 22:35:01 +0100

    Removed functionnal lexical generation, imperative suits the needs better

--- src/numtools.py
+++ src/numtools.py
@@ -47,33 +47,6 @@ def lex_perm_i(l):
         l = lcopy(lr)
     return b
 
-def lex_perm(l, b):
-    l = lcopy(l)
-    x = -1
-    y = -1
-    ll = len(l)
-    lr = []
-    for i in range(0, ll - 1):
-        if (l[i] < l[i + 1]):
-            x = i
-    if x == -1:
-        return
-
-    for i in range(0, ll):
-        if l[x] < l[i]:
-            y = i
-    l[x],l[y] = l[y],l[x]
-
-    i = 0
-    while i <= x:
-        lr.append(l[i])
-        i += 1
-
-    for i in range(0, ll - x - 1):
-        lr.append(l[ll - 1 - i])
-    b.append(lr)
-    lex_perm(lr, b)
-
 def group(l):
     gr = []
     for i in range(0, len(l)):
